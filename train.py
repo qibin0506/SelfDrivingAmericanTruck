52797a079addb27a336c74a35b5cd62d002485c6
qibin <qibin01@baidu.com>
2020-12-20 20:06:20 +0800

    update

--- train.py
+++ train.py
@@ -8,13 +8,13 @@ model = create_model(True)
 optimizer = tf.keras.optimizers.Adam(utils.lr)
 loss_obj = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
 loss_metric = tf.keras.metrics.Mean("loss_metric")
-accuracy_metric = tf.keras.metrics.CategoricalCrossentropy(name="accuracy_metric", from_logits=True)
+accuracy_metric = tf.keras.metrics.CategoricalAccuracy(name="accuracy_metric")
 
 
 ckpt_path = "./ckpt/train/"
 ckpt = tf.train.Checkpoint(
-    model = model,
-    optimizer = optimizer
+    model=model,
+    optimizer=optimizer
 )
 
 ckpt_manger = tf.train.CheckpointManager(ckpt, ckpt_path, max_to_keep=None)
@@ -36,10 +36,13 @@ def train_step(images, maps, keys):
     accuracy_metric(keys, pred)
 
 
-batch_fn = get_batch_fn(utils.batch_size)
+batch_fn, batch_count = get_batch_fn(utils.batch_size)
 for epoch in range(utils.epochs):
+    cur_batch = 0
     for images, maps, keys in batch_fn():
         train_step(images, maps, keys)
+        print("epoch: {}, batch: {}/{}, loss: {}, accuracy: {}".
+              format(epoch, cur_batch, batch_count, loss_metric.result(), accuracy_metric.result()))
+        cur_batch += 1
 
     save_path = ckpt_manger.save()
-    print("epoch: {}, loss: {}, accuracy: {}".format(epoch, loss_metric.result(), accuracy_metric.result()))
