60eead4ea71ad5aa19495cc767ea0e3a0b919286
qibin <qibin01@baidu.com>
2021-01-05 17:38:28 +0800

    using model(inputs, training=False) instead model.predict

--- predict.py
+++ predict.py
@@ -23,7 +23,6 @@ class Predict(object):
     def start(self):
         img_seq = []
         while True:
-            # start_time = time.time()
             img, _ = capture(region=self.region, dump=False)
             image, map = self.__split_and_save(img)
 
@@ -40,12 +39,26 @@ class Predict(object):
             img_seq_input = np.array([img_seq])
             map_input = np.array([map])
 
+            # start_time = time.time()
+
+            """
+            Computation is done in batches. This method is designed for performance in
+            large scale inputs. For small amount of inputs that fit in one batch,
+            directly using `__call__` is recommended for faster execution, e.g.,
+            `model(x)`, or `model(x, training=False)` if you have layers such as
+            `tf.keras.layers.BatchNormalization` that behaves differently during
+            inference. Also, note the fact that test loss is not affected by
+            regularization layers like noise and dropout.
+            """
+            # pred = self.model.predict([img_seq_input, map_input])[0]
+
             # [(batch, seq, img_height, img_width, channel) (batch, map_height, map_width, channel)]
-            pred = self.model.predict([img_seq_input, map_input])[0]
+            pred = self.model([img_seq_input, map_input], training=False).numpy()[0]
             max_index = np.argmax(pred)
+            # print("predict speed {} s".format(time.time() - start_time))
+
             key = get_key(max_index)
             press(key)
-            # print("predict speed {} s".format(time.time() - start_time))
 
     def __split_and_save(self, img):
         image = img.crop(self.image_box)
