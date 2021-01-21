93e332b159a2eb138d724ef80c3d006e22c1e16c
qibin01 <qibin01@baidu.com>
2021-01-21 10:04:45 +0800

    update image size

--- utils.py
+++ utils.py
@@ -3,10 +3,10 @@ default_image_box = [0, 272, 1606, 626] # left, top, right, bottom
 default_map_box = [1218, 752, 1572, 854] # left, top, right, bottom
 
 image_height = 66
-image_width = 200
+image_width = 300
 
-map_height = 16
-map_width = 50
+map_height = 20
+map_width = 66
 
 image_seq_size = 5
 pred_skip_frame = 7 # train_record_time(0.2) / pred_time(0.03)
@@ -18,6 +18,7 @@ n_classes = 9
 
 ckpt_path = './ckpt/train/'
 model_path = 'tf_model'
+
 
 def get_lr(epoch):
     return 1e-4
